bd00d4c47cfc964bd98d6ad2ec9a5028f91d03f4
Anhad Jain <anhadjain17@gmail.com>
2026-04-19 14:08:20 -0700

    Refactor login function to be asynchronous

--- login.js
+++ login.js
@@ -1,10 +1,25 @@
 const btn = document.getElementById("loginactivate");
 btn.addEventListener("click", login);
 
-function login() {
+async function login() {
   const pin = document.getElementById("pinf").value;
   const pass = document.getElementById("passf").value;
+  const err = document.getElementById("errfld");
 
-  document.getElementById("errfld").innerText =
-    "PIN: " + pin + " | Pass: " + pass;
+  try {
+    const res = await fetch("users.json");
+    const users = await res.json();
+
+    // check if PIN exists
+    if (users[pin] && users[pin].passcode === pass) {
+      // success → go to portal
+      window.location.href = "mainpage.html";
+    } else {
+      // wrong login
+      err.innerText = "Wrong PIN or Passcode";
+    }
+
+  } catch (e) {
+    err.innerText = "Error loading users";
+  }
 }
